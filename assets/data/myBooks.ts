c6f4ec556cab191f8e65176a2ffd86559d07e12e
kaur4542 <52495940+kaur4542@users.noreply.github.com>
2019-07-23 19:25:49 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,11 @@
+import {BookInfo} from '../../app/bookInfo';
+
+export const MYBOOKS : BookInfo [] =
+[
+   
+    {title: 'Web Dev ', author: 'M B', genre: 'Horror', year: '11-2-2014',picture:"assets/images/book1.jpg"},
+    {title: 'Networking', author: 'A B', genre: 'Academic', year: '11-2-2014',picture:"assets/images/book2.jpg"},
+    {title: 'Java 3', author: 'Mon B', genre: 'Action', year: '11-2-2014',picture:"assets/images/book3.jpg"},
+    {title: 'Database', author: 'Harry ', genre: 'Horror', year: '11-2-2014',picture:"assets/images/book4.jpg"}
+    
+]
